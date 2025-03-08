30617d708df1e897aba219c2e037d6be3b460a4d
ZennGeeraerts <76271773+ZennGeeraerts@users.noreply.github.com>
2025-03-09 00:22:19 +0100

    Initial commit

---
+++ GameServer/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace GameServer
+{
+    class Player
+    {
+        public int id;
+        public string username;
+
+        public float inputHorizontal;
+        public float inputVertical;
+        public Vector3 position;
+        public Quaternion rotation;
+
+        private float moveSpeed = 5f / Constants.TICKS_PER_SEC;
+
+        public Player(int _id, string _username, Vector3 _spawnPosition)
+        {
+            id = _id;
+            username = _username;
+            position = _spawnPosition;
+            rotation = Quaternion.Identity;
+        }
+
+        public void Update()
+        {
+            Vector2 _inputDirection = Vector2.Zero;
+
+            _inputDirection.X -= inputHorizontal;
+            _inputDirection.Y += inputVertical;
+
+            Move(_inputDirection);
+        }
+
+        private void Move(Vector2 _inputDirection)
+        {
+            Vector3 _forward = Vector3.Transform(new Vector3(0, 0, 1), rotation);
+            Vector3 _right = Vector3.Normalize(Vector3.Cross(_forward, new Vector3(0, 1, 0)));
+
+            Vector3 _moveDirection = _right * _inputDirection.X + _forward * _inputDirection.Y;
+            position += _moveDirection * moveSpeed;
+
+            ServerSend.PlayerPosition(this);
+            ServerSend.PlayerRotation(this);
+        }
+
+        public void SetInput(float _inputHorizontal, float _inputVertical, Quaternion _rotation)
+        {
+            inputHorizontal = _inputHorizontal;
+            inputVertical = _inputVertical;
+            rotation = _rotation;
+        }
+    }
+}
---
+++ GameServer/ServerHandle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace GameServer
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}");
+            if (_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+            }
+            Server.clients[_fromClient].SendIntoGame(_username);
+        }
+
+        public static void PlayerInputReceived(int _fromClient, Packet _packet)
+        {
+        
+            float _inputHorizontal = _packet.ReadFloat();
+            float _inputVertical = _packet.ReadFloat();
+            Quaternion _rotation = _packet.ReadQuaternion();
+
+            if (Server.clients[_fromClient].player != null)
+            {
+                Server.clients[_fromClient].player.SetInput(_inputHorizontal, _inputVertical, _rotation);
+            }
+        }
+    }
+}
